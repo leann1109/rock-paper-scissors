35e2b000bfd02cdc80a7b5e079d90030459bd65a
Lean Obregon <lean.obregon1@gmail.com>
2023-06-23 00:09:43 -0400

    added game function

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function getComputerChoice() {
     let randomResult = Math.floor(Math.random() * 3);
-    let choice = "";
     if (randomResult == 0) {
-        return choice = "rock";
+        return "rock";
     }
     else if (randomResult == 1) {
-        return choice = "paper";
+        return "paper";
     }
     else {
-        return choice = "scissors";
+        return "scissors";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection == "paper" && playerSelection == "rock"
+        || computerSelection == "Paper" && playerSelection == "rock"
+        || computerSelection == "PAPER" && playerSelection == "rock") {
+          return "You Lose! Paper beats Rock";
     }
 }
